a1684d02ea0ffc4c96bca3fd91cf6422b0851365
Vanina Clara <vaninamilov@gmail.com>
2025-04-03 18:52:12 +0200

    Menu burguer funciona

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const toggle = document.querySelector(".menu-toggle");
-    const navLinks = document.querySelector(".nav-links");
-  
-    if (toggle && navLinks) {
-      toggle.addEventListener("click", () => {
-        navLinks.classList.toggle("show");
-      });
-    } else {
-      console.error("⚠ No se encontraron elementos .menu-toggle o .nav-links");
-    }
+document.addEventListener('DOMContentLoaded', () => {
+  const toggle = document.querySelector('.menu-toggle');
+  const navLinks = document.querySelector('.nav-links');
+
+  toggle.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
   });
-  
+});
+
+
