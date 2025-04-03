1ec58f87e8fa4dec4a6f9a22428be2896c47bc8f
Vanina Clara <vaninamilov@gmail.com>
2025-04-03 21:38:26 +0200

    Tarjeta emergencias con javascript

--- js/main.js
+++ js/main.js
@@ -7,4 +7,14 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('componentes/emergencia.html')
+    .then(response => response.text())
+    .then(data => {
+      const placeholder = document.getElementById('emergencia-placeholder');
+      if (placeholder) {
+        placeholder.innerHTML = data;
+      }
+    });
+});
 
